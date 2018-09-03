152470274262af23e1e25e1806d06294fae01589
Robin Lovelace <rob00x@gmail.com>
2018-09-03 10:57:17 +0100

    Update crashing vignette

--- courses/rss.R
+++ courses/rss.R
@@ -1,1 +1,27 @@
 # Code demonstrating how to answer questions arisng during the course
+
+# - Find the slides here:
+# https://github.com/geocompr/geocompkg/tree/master/slides
+
+# Agenda:
+
+
+# Intro + R's spatial ecosystem: (10:00 - 10:30)
+#         See section 1.4 of Geocomputation with R
+#     Working with attribute data (10:30 - 11:00)
+#         Section 3.2 of handouts
+#
+# Coffee break: (11:00 - 11:15)
+#
+#     Spatial data and the tidyverse lecture (11:15 - 11:30)
+#     Vector data classes
+#         Online tutorial: 2.1 of Geocomputation with R (11:30 - 12:00)
+#
+#     Raster data classes and functions lecture (12:00 - 12:15)
+#         Online tutorial: 2.2 of Geocomputation with R
+#
+#     Practical working (12:15 - 13:00):
+#         Working-through code and Exercises in Chapter 2 of 'geocompr'
+#         Identify pitfalls of tidyverse
+#
+# LUNCH and discussing at your data (13:00 - 14:00)
