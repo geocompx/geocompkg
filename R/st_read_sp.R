8f55c05b5c4086c9df2099e71af2510014c1d8dc
jn <tupiszakaczki@gmail.com>
2018-09-05 15:08:53 +0200

    dontrun

--- R/st_read_sp.R
+++ R/st_read_sp.R
@@ -1,15 +1,17 @@
 #' Fast reading of spatial data into Spatial classes
-#' 
-#' @description 
+#'
+#' @description
 #' Wrapper function to read a file with [sf::st_read()] and the convert the result to a `Spatial` data class
-#' 
+#'
 #' @param dsn The path of the data source
 #' @param ... Arguments passed to `sf::st_read()`
 #' @export
 #' @md
-#' @examples 
+#' @examples
+#' \dontrun{
 #' x = st_read_sp(system.file("shapes/wrld.gpkg", package = "spData"))
 #' class(x)
+#' }
 st_read_sp = function(dsn, ...) {
   x = sf::st_read(dsn, ...)
   as(object = x, Class = "Spatial")
